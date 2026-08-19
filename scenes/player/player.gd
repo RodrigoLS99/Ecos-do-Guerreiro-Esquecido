dd510f1ff0ecdb3ae577efe1f1df574a340089b0
Ricardo <24703825+RickyM7@users.noreply.github.com>
2026-08-19 10:04:33 -0300

    feat: add. pulo e gravidade

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
 const SPEED := 160.0
+const JUMP_VELOCITY := -320.0
+const GRAVITY := 900.0
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	elif Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+
 	var direction := Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * SPEED
-	velocity.y = 0.0
 	move_and_slide()
